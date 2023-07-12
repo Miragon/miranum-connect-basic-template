3dc9166ce257cf53ecc146d5b2371f21924ed289
Rami Nakkar <rami.nakkar@miragon.io>
2023-07-12 16:22:57 +0200

    feat: add raw spring project

---
+++ src/main/java/io/miragon/example/MiranumConnectBasicTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.miragon.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiranumConnectBasicTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiranumConnectBasicTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/miragon/example/MiranumConnectBasicTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miragon.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiranumConnectBasicTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
